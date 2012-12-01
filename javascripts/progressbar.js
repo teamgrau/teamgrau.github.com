6f30d89feb04cdbc117ef95b93e55dd39880960d
Thomas Eichinger <thomas.eichinger1@gmail.com>
2012-12-01 22:46:17 +0100

    further work

---
+++ javascripts/progressbar.js
@@ -0,0 +1,110 @@
+(function($) {
+
+$.widget("ui.progressbar", {
+
+	_init: function() {
+
+		this.element
+			.addClass("ui-progressbar"
+				+ " ui-widget"
+				+ " ui-widget-content"
+				+ " ui-corner-all")
+			.attr({
+				role: "progressbar",
+				"aria-valuemin": this._valueMin(),
+				"aria-valuemax": this._valueMax(),
+				"aria-valuenow": this._value()
+			});
+
+		this.valueDiv = $('<dl><dt>Completed:</dt><dd class="ui-progressbar-value ui-widget-header ui-corner-left done"></dd><dt>Remaining:</dt><dd class="remain"></dd></dl>').appendTo(this.element);
+
+		this._refreshValue();
+
+	},
+
+	destroy: function() {
+
+		this.element
+			.removeClass("ui-progressbar"
+				+ " ui-widget"
+				+ " ui-widget-content"
+				+ " ui-corner-all")
+			.removeAttr("role")
+			.removeAttr("aria-valuemin")
+			.removeAttr("aria-valuemax")
+			.removeAttr("aria-valuenow")
+			.removeData("progressbar")
+			.unbind(".progressbar");
+
+		this.valueDiv.remove();
+
+		$.widget.prototype.destroy.apply(this, arguments);
+
+	},
+
+	value: function(newValue) {
+		if (newValue === undefined) {
+			return this._value();
+		}
+
+		this._setData('value', newValue);
+		return this;
+	},
+
+	_setData: function(key, value) {
+
+		switch (key) {
+			case 'value':
+				this.options.value = value;
+				this._refreshValue();
+				this._trigger('change', null, {});
+				break;
+		}
+
+		$.widget.prototype._setData.apply(this, arguments);
+
+	},
+
+	_value: function() {
+
+		var val = this.options.value;
+		if (val < this._valueMin()) val = this._valueMin();
+		if (val > this._valueMax()) val = this._valueMax();
+
+		return val;
+
+	},
+
+	_valueMin: function() {
+		var valueMin = 0;
+		return valueMin;
+	},
+
+	_valueMax: function() {
+		var valueMax = 100;
+		return valueMax;
+	},
+
+	_refreshValue: function() {
+		var value = this.value();
+		this.valueDiv[value == this._valueMax() ? 'addClass' : 'removeClass']("ui-corner-right");
+
+		$(this.valueDiv).find('dd.done').width(value + '%');
+		$(this.valueDiv).find('dd.done').text(value + '%');
+
+		$(this.valueDiv).find('dd.remain').width((100 - value) + '%');
+		$(this.valueDiv).find('dd.remain').text((100 - value) + '%');
+
+		this.element.attr("aria-valuenow", value);
+	}
+
+});
+
+$.extend($.ui.progressbar, {
+	version: "1.7.2",
+	defaults: {
+		value: 0
+	}
+});
+
+})(jQuery);
